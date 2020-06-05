4d6da33bd4b3288c64b648f5cd3680e5e0eaa225
RicardoWorf <steve4137@gmail.com>
2020-06-04 21:00:24 -0300

    Script para buscar filmes

---
+++ src/js/search.js
@@ -0,0 +1,19 @@
+const submitSearchFilm = document.querySelector('.searchFilms form');
+
+submitSearchFilm.onsubmit = function (e) {
+	e.preventDefault();
+	const search = document.querySelector('#search-input').value;
+
+	const withoutPrimaryLetter = search.slice(1);
+	const camelCase = search.charAt(0).toUpperCase() + withoutPrimaryLetter; 
+
+	const PATTERN = RegExp(camelCase);
+	const filtered = films.filter((filmsFiltereds) => ( PATTERN.test(filmsFiltereds.name_film) ));
+
+	if(filtered.length !== 0 ) {
+		insertInHTMLwithNameAndCategory(filtered);		
+	} else {
+		alert('test')
+	}
+
+}
